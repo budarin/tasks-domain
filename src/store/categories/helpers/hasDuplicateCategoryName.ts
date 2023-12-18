3138e8c246d53af87f8f7e0deee14c03c7d16b7d
Vadim Budarin <budarin-vv@yandex.ru>
2023-12-19 00:01:39 +0300

    fix update entity logic

--- src/store/categories/helpers/hasDuplicateCategoryName.ts
+++ src/store/categories/helpers/hasDuplicateCategoryName.ts
@@ -3,7 +3,7 @@ import type { Category } from '../../../entities/index.ts';
 
 export function hasDuplicateCategoryName(state: TasksStoreState, category: Category): boolean {
     for (const id in state.categories.byId) {
-        if (state.categories.byId[id].category_name === category.category_name) {
+        if (state.categories.byId[id].category_name === category.category_name && Number(id) !== category.category_id) {
             return true;
         }
     }
