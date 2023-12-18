a243a01565270ce4ac16662078103ae18e7bdb04
Vadim Budarin <budarin-vv@yandex.ru>
2023-12-18 23:23:49 +0300

    fix task update logic

--- src/store/tasks/updateTask.ts
+++ src/store/tasks/updateTask.ts
@@ -9,8 +9,6 @@ import { isPriorityAbsent } from './helpers/isPriorityAbsent.js';
 import { isCategoryAbsent } from './helpers/isCategoryAbsent.js';
 import { createStoreMethod } from '../_helpers/createStoreMethod.js';
 import { createExtendedTask } from './helpers/createExtendedTask.js';
-import { hasDuplicateTaskId } from './helpers/hasDuplicateTaskId.js';
-import { handleDuplicateTaskId } from './helpers/handleDuplicateTaskId.js';
 import { handlePriorityAbsence } from './helpers/handlePriorityAbsence.js';
 import { handleCategoryAbsence } from './helpers/handleCategoryAbsence.js';
 
@@ -32,10 +30,6 @@ function updateState(state: TasksStoreState, task: Task): void {
 
 function updateTasksStore(task: Task): ResultOrError<Task> {
     const state = store.getState();
-
-    if (hasDuplicateTaskId(state, task)) {
-        return handleDuplicateTaskId(task);
-    }
 
     if (isPriorityAbsent(state, task)) {
         return handlePriorityAbsence(task);
