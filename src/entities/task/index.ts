ee585e82c8513293f637b74f1bb0af9952bd70de
Vadim Budarin <budarin-vv@yandex.ru>
2024-05-08 21:47:43 +0300

    fix

--- src/entities/task/index.ts
+++ src/entities/task/index.ts
@@ -68,7 +68,7 @@ export const taskFormFieldsProps = {
         title: tsakTitle,
         name: 'title',
         type: 'text',
-        pattern: /^[a-zA-Z0-9]{title_min_length}.*/,
+        pattern: `^[a-zA-Z0-9]{${title_min_length}}.*`,
         required: true,
         minLength: title_min_length,
         maxLength: title_max_length,
