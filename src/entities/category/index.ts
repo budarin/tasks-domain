59ecbe1e730dd467fdfe1a565fbed9613f2394e8
vadim_budarin <budarin-vv@yandex.ru>
2024-05-10 14:52:42 +0300

    fix

--- src/entities/category/index.ts
+++ src/entities/category/index.ts
@@ -39,7 +39,7 @@ export const categoryFormFieldsProps = {
         name: 'category_name',
         type: 'text',
         pattern: `^[a-zA-Zа-яА-Я0-9]{${category_name_min_length}}.*`,
-        spellcheck: false,
+        spellCheck: false,
         required: true,
         minLength: category_name_min_length,
         maxLength: category_name_Max_Length,
